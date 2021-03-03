207ef055386de7fe66bb2397eb628260d5fd8b2f
askmalord <malorod99@mail.ru>
2021-03-03 21:26:27 +0300

    Task2 added

---
+++ Education.java
@@ -0,0 +1,8 @@
+package java_core_block.homeWork2.task_2;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ Sex.java
@@ -0,0 +1,6 @@
+package java_core_block.homeWork2.task_2;
+
+public enum Sex {
+    MAN,
+    WOMEN
+}
